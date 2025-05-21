6fed84357da322b632993f1eedfa58797d3cd47f
Andrew Li <andrewli@u.northwestern.edu>
2025-05-20 22:24:16 -0500

    add photos page and masonry library

--- src/pages/PhotosPage.tsx
+++ src/pages/PhotosPage.tsx
@@ -1,9 +1,20 @@
 import "./pages.scss";
 
+import Masonry from 'react-masonry-css';
+import { S3_BUCKET_URL, IMAGE_URLS } from "@/data/photos.ts";
+
 export const PhotosPage: React.FC = () => {
   return (
     <div className="page__body" id="page__404body">
-      <div id="content">This page is work in progress!</div>
+      <Masonry
+        breakpointCols={{ default: 4, 1024: 3, 768: 2, 480: 1 }}
+        className="page__photogrid"
+        columnClassName="page__photogrid__column"
+      >
+        {IMAGE_URLS
+          .sort((_, __) => Math.random() - 0.5)
+          .map(img => <img src={`${S3_BUCKET_URL}/${img}`} />)}
+      </Masonry>
     </div>
   );
 };
