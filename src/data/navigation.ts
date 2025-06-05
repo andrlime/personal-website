8ebf21a8bf1ed952e89ebff5d4a689485894badc
Andrew Li <andrewli@u.northwestern.edu>
2025-06-05 02:06:07 -0400

    update resume links

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -4,5 +4,5 @@ export interface INavigationItem {
 }
 
 // export const RESUME_LINK: string = "./resume.pdf";
-export const QD_RESUME_LINK: string = "https://drive.google.com/file/d/1Uv-47uv2fvhak16yV1zGq32_vTd7u3rB/view";
-export const SWE_RESUME_LINK: string = "https://drive.google.com/file/d/1Z5rgLueYFSrMG86OxyEhyAfTeWBIwftS/view";
+export const QD_RESUME_LINK: string = "https://drive.google.com/file/d/1z9LJ5BaIqDDl6HuUoEuFF4YQtjLSgwKN/view";
+export const SWE_RESUME_LINK: string = "https://drive.google.com/file/d/1R4_ioCCwSci9o--UdZl7voIAOti2LVm0/view";
