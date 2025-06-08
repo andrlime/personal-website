fd5e906fb3f7c9c57af16f40f35a7c076e3b469c
Andrew Li <andrewli@u.northwestern.edu>
2025-06-08 10:21:56 -0500

    update experiences

--- src/components/experience/ExperienceBox.tsx
+++ src/components/experience/ExperienceBox.tsx
@@ -5,8 +5,8 @@ export const ExperienceBox = () => (
   <div className="experience_box__box">
     <ExperienceRow icon="capitalone" company="Capital One" title="SWE Intern - Synthetic Testing" time="June 2025 – Present" />
     <ExperienceRow icon="northwestern" company="Northwestern University" title="Undergrad TA" time="September 2023 – Present" />
-    <ExperienceRow icon="nhsdlc" company="National HS Debate League of China" title="Software Dev" time="December 2022 – Present" />
     <ExperienceRow icon="nuft" company="Northwestern Fintech Club" title="Core Software Dev" time="October 2022 – Present" />
+    <ExperienceRow icon="nhsdlc" company="National HS Debate League of China" title="Software Dev & Debate Coach" time="April 2022 – Present" />
     <ExperienceRow icon="wildhacks" company="Northwestern Wildhacks" title="Director of Tech" time="April 2023 – May 2025" />
     <ExperienceRow icon="capitalone" company="Capital One" title="SWE Intern - Cloud Infrastructure" time="June 2024 – August 2024" />
     <ExperienceRow icon="draper" company="Draper Laboratory" title="SWE Intern - Frontend" time="June 2023 – August 2023" />
